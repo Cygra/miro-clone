18ebbd43d886ba4cf30df146bbbd8049218bb6a1
Cygra <sjtuwbh@gmail.com>
2024-12-27 16:24:23 +0800

    chore: add convex domain in .enx.example

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
     providers: [
         {
-            domain: "https://regular-humpback-73.clerk.accounts.dev",
+            domain: process.env.CLERK_DOMAIN,
             applicationID: "convex",
         },
     ],
